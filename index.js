d55b7de08923341812599f437e09e6c9523fd0c5
Haider Mehdi <haidershah659@gmail.com>
2023-10-18 18:33:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Personal Message
+
+const firstname = 'Eric';
+console.log("Hello " + firstname + ", would you like to learn some Python today?");
---
+++ index.ts
@@ -0,0 +1,4 @@
+// Personal Message
+
+const firstname = 'Eric';
+console.log("Hello" + firstname + "would you like to learn some Python today?");
